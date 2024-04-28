6733c0e608275933d5279ecc4da8937703e02100
timhams <timtam54@hotmail.com>
2024-04-29 07:52:35 +1000

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,113 +1,195 @@
-import Image from "next/image";
-
-export default function Home() {
-  return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <div className="z-10 w-full max-w-5xl items-center justify-between font-mono text-sm lg:flex">
-        <p className="fixed left-0 top-0 flex w-full justify-center border-b border-gray-300 bg-gradient-to-b from-zinc-200 pb-6 pt-8 backdrop-blur-2xl dark:border-neutral-800 dark:bg-zinc-800/30 dark:from-inherit lg:static lg:w-auto  lg:rounded-xl lg:border lg:bg-gray-200 lg:p-4 lg:dark:bg-zinc-800/30">
-          Get started by editing&nbsp;
-          <code className="font-mono font-bold">app/page.tsx</code>
-        </p>
-        <div className="fixed bottom-0 left-0 flex h-48 w-full items-end justify-center bg-gradient-to-t from-white via-white dark:from-black dark:via-black lg:static lg:size-auto lg:bg-none">
-          <a
-            className="pointer-events-none flex place-items-center gap-2 p-8 lg:pointer-events-auto lg:p-0"
-            href="https://vercel.com?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            By{" "}
-            <Image
-              src="/vercel.svg"
-              alt="Vercel Logo"
-              className="dark:invert"
-              width={100}
-              height={24}
-              priority
-            />
-          </a>
-        </div>
-      </div>
-
-      <div className="relative z-[-1] flex place-items-center before:absolute before:h-[300px] before:w-full before:-translate-x-1/2 before:rounded-full before:bg-gradient-radial before:from-white before:to-transparent before:blur-2xl before:content-[''] after:absolute after:-z-20 after:h-[180px] after:w-full after:translate-x-1/3 after:bg-gradient-conic after:from-sky-200 after:via-blue-200 after:blur-2xl after:content-[''] before:dark:bg-gradient-to-br before:dark:from-transparent before:dark:to-blue-700 before:dark:opacity-10 after:dark:from-sky-900 after:dark:via-[#0141ff] after:dark:opacity-40 sm:before:w-[480px] sm:after:w-[240px] before:lg:h-[360px]">
-        <Image
-          className="relative dark:drop-shadow-[0_0_0.3rem_#ffffff70] dark:invert"
-          src="/next.svg"
-          alt="Next.js Logo"
-          width={180}
-          height={37}
-          priority
-        />
-      </div>
-
-      <div className="mb-32 grid text-center lg:mb-0 lg:w-full lg:max-w-5xl lg:grid-cols-4 lg:text-left">
-        <a
-          href="https://nextjs.org/docs?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Docs{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Find in-depth information about Next.js features and API.
-          </p>
-        </a>
-
-        <a
-          href="https://nextjs.org/learn?utm_source=create-next-app&utm_medium=appdir-template-tw&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Learn{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Learn about Next.js in an interactive course with&nbsp;quizzes!
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/templates?framework=next.js&utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Templates{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-sm opacity-50">
-            Explore starter templates for Next.js.
-          </p>
-        </a>
-
-        <a
-          href="https://vercel.com/new?utm_source=create-next-app&utm_medium=appdir-template&utm_campaign=create-next-app"
-          className="group rounded-lg border border-transparent px-5 py-4 transition-colors hover:border-gray-300 hover:bg-gray-100 hover:dark:border-neutral-700 hover:dark:bg-neutral-800/30"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          <h2 className="mb-3 text-2xl font-semibold">
-            Deploy{" "}
-            <span className="inline-block transition-transform group-hover:translate-x-1 motion-reduce:transform-none">
-              -&gt;
-            </span>
-          </h2>
-          <p className="m-0 max-w-[30ch] text-balance text-sm opacity-50">
-            Instantly deploy your Next.js site to a shareable URL with Vercel.
-          </p>
-        </a>
-      </div>
-    </main>
-  );
+'use client'
+import 'react-big-calendar/lib/css/react-big-calendar.css'
+import { Calendar, dateFnsLocalizer } from 'react-big-calendar'
+import format from 'date-fns/format'
+import parse from 'date-fns/parse'
+import startOfWeek from 'date-fns/startOfWeek'
+import getDay from 'date-fns/getDay'
+import enUS from 'date-fns/locale/en-AU'
+import "react-big-calendar/lib/css/react-big-calendar.css";
+import React, { ChangeEvent, useState  }  from 'react';
+import Link from 'next/link'
+import ArrowBackIcon from '@mui/icons-material/ArrowBack';
+const locales = {
+  'en-US': enUS,
 }
+
+const localizer = dateFnsLocalizer({
+  format,
+  parse,
+  startOfWeek,
+  getDay,
+  locales,
+})
+
+interface eventObject{
+  id:number;
+  title:string;
+  start:Date;
+  end:Date;
+  resourceId:number;
+  colorEvento:string;
+  color:string;
+}
+
+
+interface ResourceRow{
+  resourceid:string;
+  resourcetitle:string;
+}
+/*type eventUser = {
+  id: number;
+  title: string;
+  start: Date;
+  end:Date;
+  resourceId:number;
+};
+*/
+/*type eventServer = {
+  id: number;
+  title: string;
+  start: string;
+  end:string;
+  resourceId:number;
+};*/
+/*const decodeUser = (dto: eventServer): eventUser => {
+  return {
+    id: dto.id,
+    title: dto.title,
+    start: new Date(dto.start),
+    end: new Date(dto.end),
+    resourceId: dto.resourceId,
+  };*/
+
+export default  function  Page()
+{
+  const api=true;
+  const EngID="0";
+  const [resourceMap,setResourceMap] = useState<ResourceRow[]>([]);
+  const [engineers,setEngineers] = useState<ResourceRow[]>([]);
+  const [engineerID,setEngineerID] = useState(EngID);
+  
+  const [events,setEvents] = useState<eventObject[]>([]);//repairList
+  
+
+ 
+    React.useEffect(()=>{
+      fetchEngineer();
+      
+    },[]);
+
+    const fetchEngineer = async()=>{
+    
+      const endpoint = (api)?'https://mdsapi.icyforest-7eae763b.australiaeast.azurecontainerapps.io/api/TechEngineerForDiary/{id}?EngineerID='+EngID: '/data-api/rest/TechEngineerForDiary?EngineerID='+EngID;
+      const response = await fetch(endpoint);
+      console.log(endpoint);
+      const data = await response.json();
+      const result = (api)?data:data.value;
+      setEngineers(result);
+      //setResourceMap(result.filter((e:ResourceRow) => e.resourceid==EngID));
+      if (EngID=="0")
+        {
+          setResourceMap(result);
+        }
+        else
+        {
+          setResourceMap(result.filter((e:ResourceRow) => e.resourceid==EngID));
+        }
+      fetchTechScheduler(EngID.toString());
+   }
+    const fetchTechScheduler = async(engid:string)=>{
+      try{
+        console.log(engid);
+      const endpoint =  (api)?'https://mdsapi.icyforest-7eae763b.australiaeast.azurecontainerapps.io/api/TechScheduler/{id}?EngineerID='+engid:'/data-api/rest/TechScheduler?EngineerID='+engid;
+      console.log(endpoint);
+      const response = await fetch(endpoint);
+      const data = await response.json();
+      const result = (api)?data:data.value;
+      if (result==null)
+        {
+          setEvents([]);
+          return;
+        }
+      let myevent: eventObject[] = [];
+      for(let i=0;i<result.length ;i++)
+        {
+          myevent.push({color:result[i].color,colorEvento:result[i].colorevento,id:result[i].id,title:result[i].title,start:new Date(result[i].start),end:new Date(result[i].end),resourceId:result[i].resourceid});
+        }
+
+      console.table(myevent);
+      setEvents(myevent);
+      
+      }
+      catch (error)
+      {
+        console.error('Error: '+error)
+      }
+      
+    }
+
+    const engChange = (event: ChangeEvent<HTMLSelectElement>) => { // <----- here we assign event to ChangeEvent
+      
+      console.log(event.target.value); // Example: Log the value of the selected option
+      setEngineerID(event.target.value);
+      fetchTechScheduler(event.target.value);
+      if (event.target.value=="0")
+        {
+          setResourceMap(engineers);
+        }
+        else
+        {
+          setResourceMap(engineers.filter(e => e.resourceid==event.target.value));
+        }
+    };
+
+   
+
+    return (
+      <>
+      <div className="bg-white p-2 text-red text-3xl header" >
+        <img style={{height:"83px",width:"217px"}}
+              src="/logo.png"/>              
+                <div>
+                   <Link href="/"><ArrowBackIcon/></Link>
+                   </div>
+<div>
+<select  onChange={engChange}>
+      <option key='0' value='0'>All</option>
+        {engineers.map((eng, index) => {
+      return (
+        <option key={eng.resourceid} value={eng.resourceid} selected={eng.resourceid==engineerID}>
+          {eng.resourcetitle}
+        </option>
+      );
+      
+    })}
+        </select>
+  </div>
+             <h1><b>Technician Diary</b></h1>
+        </div>
+        <div className="App">
+      
+      <Calendar
+       style={{ height: 500 }}
+        defaultView='week'
+        resourceIdAccessor="resourceid"
+        resourceTitleAccessor="resourcetitle"
+        views={['week','agenda']}
+        localizer={localizer}
+        events={events}
+        resources={resourceMap}
+        startAccessor="start"
+        endAccessor="end"
+     
+          eventPropGetter={(events) => {
+            const backgroundColor = events.colorEvento ? events.colorEvento : 'blue';
+            const color = events.color ? events.color : 'blue';
+            return { style: { backgroundColor ,color} }
+          }}
+      />
+    </div>
+    </>
+    );
+}
+
+
